38a85e022981c541c483f01eb4d814d1d7e714f5
Priyanshu Ranjan <heyapriyanshu@gmail.com>
2023-03-06 21:06:26 +0530

    Splash Done

---
+++ app/src/main/java/com/example/tic_tac_toe/splash.kt
@@ -0,0 +1,34 @@
+package com.example.tic_tac_toe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+class splash : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+
+        imageView = findViewById(R.id.imageview);
+
+        // Adding the gif here using glide library
+        Glide.with(this).load(R.drawable.splashh).into(imageView);
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+
+    }
+}
